e445d7967bd7057ba06fc74e931862f429a58fee
Danny-DeLaRosa <danny.devlarosa@gmail.com>
2025-02-20 20:37:26 -0800

    Arrays

--- src/DifferenceArraysArrayLists.java
+++ src/DifferenceArraysArrayLists.java
@@ -2,7 +2,25 @@
 public class DifferenceArraysArrayLists {
 
 	public static void main(String[] args) {
-		System.out.println("Diefference Arrays ArrayLists");
+		// Arrays
+		int n = 4;
+		
+		// Array that holds 5 integers
+		//int[] a = new int[5];
+		
+		int[] a = {2, 7, 9, 43, 78}; 
+		// assigns a number at index 3
+		a[3] = 27;
+		
+		//Arrays are not flexible so i cannot add or subtract a number from the array. 
+		a[5] = 75;
+		System.out.println(n); //prints variable 
+		System.out.println(a);// prints the location or reference of array
+		System.out.println(a[3]);// prints the location or reference of array
+		
+		
+		
+		
 
 	}
 
